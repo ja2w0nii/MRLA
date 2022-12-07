0d1aaa756ba9c4bcb94f771a838dee3245b952c6
WR-10 <sadfrog@kakao.com>
2022-12-07 11:37:01 +0900

    feat : 소셜 로그인(kakao) #6

--- users/urls.py
+++ users/urls.py
@@ -7,10 +7,11 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("signup/", views.UserView.as_view(), name="user_view"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("profile/", views.ProfileView.as_view(), name="profile_view"),
     path('google/login/', views.google_login, name='google_login'),
     path('google/callback/', views.google_callback, name='google_callback'),
     path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
+    path('kakao/login/', views.kakao_login, name='kakao_login'),
+    path('kakao/callback/', views.kakao_callback, name='kakao_callback'),
+    path('kakao/login/finish/', views.KakaoLogin.as_view(), name='kakao_login_todjango'),
 ]

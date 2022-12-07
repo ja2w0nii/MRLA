d72784a708b88932fab6d06e40255a39ef124d20
R5Z Yoon <asaucybloom@gmail.com>
2022-12-07 15:05:06 +0900

    Feat: 댓글 수정/삭제 기능 추가
    - 댓글 수정 및 삭제 가능
    
    Open #5

--- foods/urls.py
+++ foods/urls.py
@@ -4,4 +4,5 @@ from foods import views
 urlpatterns = [
     path("main/", views.FoodView.as_view(), name="food_view"),
     path("main/<int:food_id>/", views.FoodCommentView.as_view(), name="food_comment_view"),
+    path("main/<int:food_id>/comment/<int:comment_id>/", views.FoodCommentView.as_view(), name="food_comment_view"),
 ]
--- foods/views.py
+++ foods/views.py
@@ -2,7 +2,7 @@ from rest_framework import permissions, status
 from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from foods.models import Food
+from foods.models import Food, FoodComment
 from foods.serializers import FoodSerializer, FoodCommentSerializer, FoodCommentCreateSerializer
 
 
@@ -30,6 +30,26 @@ class FoodCommentView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, food_id, comment_id):
+        comment = get_object_or_404(FoodComment, id=comment_id)
+        if request.user == comment.user:
+            serializer = FoodCommentCreateSerializer(comment, data=request.data)
+            if serializer.is_valid(): 
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"message": "권한이 없습니다!"}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    def delete(self, request, food_id, comment_id):
+        comment = get_object_or_404(FoodComment, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response({"message": "삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"message": "권한이 없습니다!"}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 # 카테고리별 모아보기는 카테고리가 정리 후 추가구현 예정

87560e53c77b5f5a217f2cee975fd3392d76932d
R5Z Yoon <asaucybloom@gmail.com>
2022-12-12 20:17:53 +0900

    Refactor: views 정리, image data 추가
    - 메뉴(음식) 데이터 업데이트(이미지 데이터 추가)
    - views 정리
    
    Open #5

--- foods/models.py
+++ foods/models.py
@@ -6,12 +6,12 @@ class Food(models.Model):
     class Meta:
         db_table = "food"
 
-    food_id = models.IntegerField(primary_key=True, unique=True)
-    menu = models.CharField(verbose_name="음식명", max_length=50)
+    food_id = models.IntegerField(primary_key=True, unique=True, default="")
+    menu = models.CharField(verbose_name="음식명", max_length=50, default="")
     image = models.ImageField(verbose_name="음식 사진", blank=True)
-    major_category = models.CharField(verbose_name="대분류", max_length=50)
-    middle_category = models.CharField(verbose_name="중분류", max_length=50)
-    minor_category = models.CharField(verbose_name="소분류", max_length=50)
+    major_category = models.CharField(verbose_name="대분류", max_length=50, default="")
+    middle_category = models.CharField(verbose_name="중분류", max_length=50, default="")
+    minor_category = models.CharField(verbose_name="소분류", max_length=50, default="")
 
     likes = models.ManyToManyField(User, verbose_name="좋아요 음식", through="FoodLike", related_name="food_likes", blank=True)
 
@@ -33,7 +33,7 @@ class FoodComment(models.Model):
         db_table = "food_comment"
 
     user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE, related_name="foodcomment_user")
-    menu = models.ForeignKey(Food, verbose_name="음식명", on_delete=models.CASCADE, related_name="foodcomment_menu")
+    menu = models.ForeignKey(Food, verbose_name="음식명", on_delete=models.CASCADE, related_name="foodcomment_menu", default="")
     comment = models.TextField(verbose_name="음식 댓글", max_length=500)
     created_at = models.DateTimeField(verbose_name="작성 시간", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="수정 시간", auto_now=True)
@@ -42,17 +42,24 @@ class FoodComment(models.Model):
         return str(f"{self.user} / {self.menu} / {self.comment}")
 
 
-# 카테고리 추가될 가능성 있음
-class MainCategories(models.Model):
-    main_category = models.CharField(max_length=20)
+# # 어떤 방식으로 활용할지?
+# class MajorCategories(models.Model):
+#     major_category = models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.main_category
+#     def __str__(self):
+#         return self.major_category
 
+# class MiddleCategories(models.Model):
+#     middle_category = models.CharField(max_length=20)
+#     major_category = models.ForeignKey("MajorCategories", on_delete=models.CASCADE, default="")
 
-class SubCategories(models.Model):
-    sub_category = models.CharField(max_length=20)
-    main_category = models.ForeignKey("MainCategories", on_delete=models.CASCADE, default="")
+#     def __str__(self):
+#         return self.middle_category
 
-    def __str__(self):
-        return self.sub_category
+# class MinorCategories(models.Model):
+#     minor_category = models.CharField(max_length=20)
+#     middle_category = models.ForeignKey("MiddleCategories", on_delete=models.CASCADE, default="")
+#     major_category = models.ForeignKey("MajorCategories", on_delete=models.CASCADE, default="")
+
+#     def __str__(self):
+#         return self.minor_category
